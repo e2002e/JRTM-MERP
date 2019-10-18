96e3273d3c0bbb19fb060752aa49d079b5e7302c
owein <yvain29@gmail.com>
2019-10-19 00:48:19 +0200

    here we go english speaking fellows

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTranslator>
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTranslator translator;
+    translator.load(QLocale(), QString("translations"), QString("_"), QString(":/languages"));
+
+    a.installTranslator(&translator);
     MainWindow w;
     w.show();
 
